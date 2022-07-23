09ff3abe16fc8b439bb32f5ca9b53c0f49e8ac0c
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-07-23 09:32:20 +0200

    finish sum-all-numbers-in-range.js
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

--- sum-all-numbers-in-range.js
+++ sum-all-numbers-in-range.js
@@ -1,1 +1,19 @@
-function sumAllInrange(arr) {}
+function sumAll(arr) {
+  const arrSorted = [].concat(arr).sort((a, b) => a - b);
+  return arrSorted.reduce(
+    (sum, nb) => sum + nb + getSumBetweenNumbers(arrSorted)
+  );
+}
+
+function getSumBetweenNumbers(arr) {
+  let arrToSum = [];
+  for (let i = arr[0] + 1; i < arr[1]; i++) {
+    arrToSum.push(i);
+  }
+  return arrToSum.reduce((sum, nb) => sum + nb);
+}
+
+console.log(getSumBetweenNumbers([1, 4]));
+console.log(getSumBetweenNumbers([5, 10]));
+console.log(sumAll([1, 4]));
+console.log(sumAll([10, 5]));
