4250e8869ba0fb219d7e0044f5da0a839fd37bdc
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-07-25 11:19:20 +0200

    finish what-is-in-a-name.js
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

---
+++ what-is-in-a-name.js
@@ -0,0 +1,38 @@
+function whatIsInAName(collection, source) {
+  const sourceKeys = Object.keys(source);
+  return collection.filter((obj) => {
+    for (let i = 0; i < sourceKeys.length; i++) {
+      if (
+        !obj.hasOwnProperty(sourceKeys[i]) ||
+        obj[sourceKeys[i]] !== source[sourceKeys[i]]
+      ) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
+
+console.log(
+  whatIsInAName(
+    [
+      { first: 'Romeo', last: 'Montague' },
+      { first: 'Mercutio', last: null },
+      { first: 'Tybalt', last: 'Capulet' },
+    ],
+    { last: 'Capulet' }
+  )
+);
+
+console.log(
+  whatIsInAName([{ apple: 1 }, { apple: 1 }, { apple: 1, bat: 2 }], {
+    apple: 1,
+  })
+);
+
+console.log(
+  whatIsInAName(
+    [{ apple: 1, bat: 2 }, { bat: 2 }, { apple: 1, bat: 2, cookie: 2 }],
+    { apple: 1, bat: 2 }
+  )
+);
