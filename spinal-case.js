319544680d7bc27a132de284abbda68340a9632d
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-07-25 11:41:36 +0200

    finish spinal-case.js
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

---
+++ spinal-case.js
@@ -0,0 +1,10 @@
+function spinalCase(str) {
+  return str
+    .split(/\s|_|(?=[A-Z])/)
+    .join('-')
+    .toLowerCase();
+}
+
+console.log(spinalCase('This Is Spinal Tap'));
+console.log(spinalCase('thisIsSpinalTap'));
+console.log(spinalCase('The_Andy_Griffith_Show'));
