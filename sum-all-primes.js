3d6d873b89e3bf6c85555e32e3180803c297d4a8
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-07-28 09:08:44 +0200

    finish sum-all-primes.js
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

--- sum-all-primes.js
+++ sum-all-primes.js
@@ -1,4 +1,24 @@
-function sumPrimes(num) {}
+function sumPrimes(num) {
+  let primesNumbers = [];
+
+  for (let i = 0; i <= num; i++) {
+    if (isPrime(i)) {
+      primesNumbers.push(i);
+    }
+  }
+  return primesNumbers.reduce((primesNbSum, primeNb) => primesNbSum + primeNb);
+}
+
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num === 2) return true;
+  const sqrt = Math.sqrt(num);
+
+  for (var i = 2; i <= sqrt; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
 
 console.log(sumPrimes(10));
 console.log(sumPrimes(977));
