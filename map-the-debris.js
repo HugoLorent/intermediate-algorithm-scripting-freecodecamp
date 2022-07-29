f8fe9cbb10348bcd4397df1948bc9a6975e42d49
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-07-29 18:22:21 +0200

    finish map-the-debris.js
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

---
+++ map-the-debris.js
@@ -0,0 +1,21 @@
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  return arr.map((body) => {
+    const semiMajorAxis = earthRadius + body.avgAlt;
+    body.orbitalPeriod = Math.round(
+      2 * Math.PI * Math.sqrt(Math.pow(semiMajorAxis, 3) / GM)
+    );
+    delete body.avgAlt;
+    return body;
+  });
+}
+
+console.log(orbitalPeriod([{ name: 'sputnik', avgAlt: 35873.5553 }]));
+console.log(
+  orbitalPeriod([
+    { name: 'iss', avgAlt: 413.6 },
+    { name: 'hubble', avgAlt: 556.7 },
+    { name: 'moon', avgAlt: 378632.553 },
+  ])
+);
